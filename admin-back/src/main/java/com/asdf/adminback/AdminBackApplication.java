a7ff6271d4f8b583e09470ad59776390112d13ed
Veljko <veljkozajecar@gmail.com>
2022-04-09 17:48:29 +0200

    Admin front and back initialized.

---
+++ admin-back/src/main/java/com/asdf/adminback/AdminBackApplication.java
@@ -0,0 +1,13 @@
+package com.asdf.adminback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminBackApplication.class, args);
+	}
+
+}
---
+++ admin-back/src/test/java/com/asdf/adminback/AdminBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asdf.adminback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
